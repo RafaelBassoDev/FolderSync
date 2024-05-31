77f322aad26c5ad4e49c12fd6cb2194e64475ce0
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-05-31 14:39:24 +0200

    IFileComparisonStrategy (docs): add documentation for the 'Compare' method.

--- src/services/FileSynchronization/Interface/IFileComparisonStrategy.cs
+++ src/services/FileSynchronization/Interface/IFileComparisonStrategy.cs
@@ -1,5 +1,11 @@
 namespace FolderSync;
 
 interface IFileComparisonStrategy {
+    /// <summary>
+    /// Compares two files and checks wether they are considered equal.
+    /// </summary>
+    /// <param name="sourceFile"> the source file.</param>
+    /// <param name="targetFile"> the file to be compared.</param>
+    /// <returns>Returns <c>true</c> if the files are equal, otherwise returns <c>false</c>.</returns>
     bool Compare(string sourceFile, string targetFile);
 }
