195b743f7fddc6aab13fc24984293356bceb7259
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-05-31 16:08:25 +0200

    FileSynchronization (feat): implement file and folder pruning.

--- src/services/FileSynchronization/FileSynchronization.cs
+++ src/services/FileSynchronization/FileSynchronization.cs
@@ -13,16 +13,31 @@ namespace FileSynchronization {
         private readonly Logger? logger = logger;
 
         /// <summary>
-        /// Synchronizes all the files and folders from <c>sourceFolderPath</c> to <c>replicaFolderPath</c>.
+        /// Synchronizes all folders from <c>sourceFolderPath</c> to <c>replicaFolderPath</c>.
         /// </summary>
         public void SynchronizeFolders(string sourceFolderPath, string replicaFolderPath) {
-
-            if (!Directory.Exists(replicaFolderPath)){
+            if (!Directory.Exists(replicaFolderPath)) {
                 Directory.CreateDirectory(replicaFolderPath);
                 logger?.LogMessage("<create> <timestamp> " + replicaFolderPath);
             }
 
-            foreach (string sourceFilePath in Directory.GetFiles(sourceFolderPath)) {
+            // If the directory is empty of files/folders, exit the current iteration.
+            if (!Directory.EnumerateFileSystemEntries(sourceFolderPath).Any()) { return; }
+
+            SynchronizeFiles(sourceFolderPath, replicaFolderPath);
+
+            foreach (string sourceSubFolderPath in Directory.GetDirectories(sourceFolderPath, "*", SearchOption.AllDirectories)) {
+                string sourceSubFolderName = Path.GetFileName(sourceSubFolderPath);
+                string replicaSubFolderPath = Path.Combine(replicaFolderPath, sourceSubFolderName);
+                SynchronizeFolders(sourceSubFolderPath, replicaSubFolderPath);
+            }
+        }
+
+        /// <summary>
+        /// Synchronizes all files from <c>sourceFolderPath</c> to <c>replicaFolderPath</c>.
+        /// </summary>
+        public void SynchronizeFiles(string sourceFolderPath, string replicaFolderPath) {
+            foreach (string sourceFilePath in Directory.GetFiles(sourceFolderPath, "*")) {
                 string sourceFileName = Path.GetFileName(sourceFilePath);
                 string replicaFilePath = Path.Combine(replicaFolderPath, sourceFileName);
 
@@ -32,11 +47,40 @@ namespace FileSynchronization {
                 File.Copy(sourceFilePath, replicaFilePath, true);
                 logger?.LogMessage("<copy> <timestamp> " + sourceFilePath);
             }
+        }
 
-            foreach (string subFolderPath in Directory.GetDirectories(sourceFolderPath)) {
-                string sourceDirectoryName = Path.GetFileName(subFolderPath);
-                string replicaDirectoryPath = Path.Combine(replicaFolderPath, sourceDirectoryName);
-                SynchronizeFolders(subFolderPath, replicaDirectoryPath);
+        /// <summary>
+        /// Deletes all folders from <c>sourceFolderPath</c> that are not present in <c>targetFolderPath</c>.
+        /// </summary>
+        private void PruneFoldersFrom(string folderPath, string targetFolderPath) {
+            PruneFilesFrom(folderPath, targetFolderPath);
+
+            foreach (string subFolderPath in Directory.GetDirectories(folderPath, "*", SearchOption.AllDirectories)) {
+                string subFolderName = Path.GetFileName(subFolderPath);
+                string targetSubFolderPath = Path.Combine(targetFolderPath, subFolderName);
+
+                if (!Directory.Exists(targetSubFolderPath)) {
+                    Directory.Delete(subFolderPath, true);
+                    logger?.LogMessage("<delete> <timestamp> " + subFolderPath);
+                    continue;
+                }
+
+                PruneFoldersFrom(subFolderPath, targetSubFolderPath);
+            }
+        }
+
+        /// <summary>
+        /// Deletes all files from <c>sourceFolderPath</c> that are not present in <c>targetFolderPath</c>.
+        /// </summary>
+        private void PruneFilesFrom(string folderPath, string targetFolderPath) {
+            foreach (string filePath in Directory.GetFiles(folderPath, "*")) {
+                string fileName = Path.GetFileName(filePath);
+                string targetFilePath = Path.Combine(targetFolderPath, fileName);
+
+                if (File.Exists(targetFilePath)) { continue; }
+
+                File.Delete(filePath);
+                logger?.LogMessage("<delete> <timestamp> " + filePath);
             }
         }
 
@@ -46,6 +90,7 @@ namespace FileSynchronization {
         /// <param name="interval">The synchronization interval in seconds.</param>
         public void StartSynchronization(int interval) {
             SynchronizeFolders(sourceFolderPath, replicaFolderPath);
+            PruneFoldersFrom(replicaFolderPath, targetFolderPath: sourceFolderPath);
             Console.WriteLine($"starting synchronization with interval: {interval}");
         }
 
