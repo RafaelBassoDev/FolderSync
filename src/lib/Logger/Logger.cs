5c9435c052d7ab1d05d1a2c0d2da2385891f20f8
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-05-31 21:31:29 +0200

    Logger (refactor): improve log entry formatting.

--- src/lib/Logger/Logger.cs
+++ src/lib/Logger/Logger.cs
@@ -1,4 +1,13 @@
 namespace Logging {
+    public enum LogLabel {
+        Copy,
+        Create,
+        Delete,
+        Error,
+        Info,
+        Debug
+    }
+
     public class Logger {
         private StreamWriter? streamWriter;
 
@@ -11,8 +20,9 @@ namespace Logging {
                 this.streamWriter = new StreamWriter(outputFileUrl, true) {
                     AutoFlush = true
                 };
+
             } catch (Exception e) {
-                Console.WriteLine($"<Logger>[{e.GetType().ToString()}]: failed to create output file on '{outputFileUrl}'");
+                LogError(e, $"Failed to create output file on '{outputFileUrl}.'");
             }
         }
 
@@ -21,16 +31,18 @@ namespace Logging {
         }
 
         private void Log(string message, bool includeTimeStamp = true) {
-            streamWriter?.WriteLine($"-{(includeTimeStamp ? DateTime.UtcNow : "")}- {message}");
-            Console.WriteLine($"-{(includeTimeStamp ? DateTime.UtcNow : "")}- {message}");
+            string timeStamp = includeTimeStamp ? $"[UTC {DateTime.UtcNow.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}] " : "";
+            streamWriter?.WriteLine(timeStamp + message);
+            Console.WriteLine(timeStamp + message);
         }
 
-        public void LogMessage(string message) {
-            Log(message);
+        public void LogMessage(string message, LogLabel label = LogLabel.Info, bool includeTimeStamp = true) {
+            string labelName = Enum.GetName(typeof(LogLabel), label) ?? "";
+            Log($"{labelName.PadLeft(6).ToUpper()} - {message}", includeTimeStamp);
         }
 
         public void LogError(Exception e, string message = "") {
-            Log($"{message}\n{e.Message}\n{e.StackTrace}");
+            LogMessage($"{message} {e.GetType().FullName}: {e.Message}\n{e.StackTrace}", LogLabel.Error);
         }
 
         public void Close() {
